96d511200d19287d2b4181fb9c13546217f65d4b
agordienko <agordienko@zuzex.com>
2021-08-13 09:15:20 +0300

    initial commit

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYWdvcmRpZW5rbyIsImEiOiJja3MwOXl3ZG8wNjhkMnVxbTB0N2QwcGFvIn0.TCBtIxRVoUQE_yAlca1hfg&limit=1'
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
